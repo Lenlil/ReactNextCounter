11d0b44433d4efa48ea8e2e463f283a0a9bcf32d
Lenlil <55378996+Lenlil@users.noreply.github.com>
2021-05-26 12:01:36 +0200

    Add styling for buttons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from "react-jss"
+
+const theme = {
+  black: '#000000',
+  white: '#FFFFFF',
+  normal: 'white',
+  primary: 'hotpink',
+  hoverNormal: 'black',
+  hoverPrimary: 'rebeccapurple'
 }
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>      
+      <Component {...pageProps} />        
+    </ThemeProvider>
+  )
+}
+
+export default App;
